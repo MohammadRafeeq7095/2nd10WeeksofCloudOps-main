9e9385cf0ba69cb876655511d76dcae4bc7c10cb
MohammadRafeeq7095 <angadimohammadrafeeq@gmail.com>
2025-02-28 16:53:23 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://backend.abdullahbrand.shop";
+const API_BASE_URL = "http://a5ecbfb95fafd402498dd58901794a7a-1878662033.ap-south-1.elb.amazonaws.com";
 export default API_BASE_URL;
